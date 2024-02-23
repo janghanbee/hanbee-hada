6a304ea2505bbce2025d2a93091cf477ce823458
janghanbee <jang010605@gmail.com>
2024-02-23 11:59:43 +0900

    change react-player to react-youtube

--- src/pages/project/antifreeze.js
+++ src/pages/project/antifreeze.js
@@ -1,25 +1,37 @@
 import React from 'react';
-// import ReactPlayer from 'react-player/lazy';
+import YouTube from 'react-youtube';
+import './project.css';
 
 const Antifreeze = () => {
   return (
     <div>
-      {/* <h1 className="header">Kinetic Typography - ANTIFREEZE</h1>
+      <h1 className="header">Kinetic Typography - ANTIFREEZE</h1>
       <div className="project-container">
         <div className="project-textbox">
             <div className="project-description">
-                <div className="player">
-                    <ReactPlayer
-                        src="/img/antifreeze-1.mp4"
-                        type="video/mp4" 
-                        playing={true}
-                        muted={true}
-                        controls={true}
-                    />
-                </div>
+              <YouTube className="player"
+                  videoId="_h9zlnCNx58"
+                  opts={{
+                    width: "100%",
+                    playerVars: {
+                      autoplay: 1,
+                      rel: 0,
+                      modestbranding: 1,
+                    },
+                  }}
+                  onEnd={(e)=>{e.target.stopVideo(0);}}      
+                />
+              <br></br>
+              (Will be changed later)
+              <br></br>
+              VOICE PLANET is a personalized voice recorder that links special moments to a specific place. 
+              With the VOICE PLANET, users can capture their special moments to a specific place and share them with their loved ones. 
+              They'll only be able to hear the voice message when they visit that exact same spot, creating a one-of-a-kind experience that's sure to leave a lasting impression.
+              <br></br>
+              <br></br>
             </div>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
--- src/pages/project/voicePlanet.js
+++ src/pages/project/voicePlanet.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import YouTube from 'react-youtube';
+import './project.css';
 
 const VoicePlanet = () => {
   return (
@@ -7,17 +9,25 @@ const VoicePlanet = () => {
       <div className="project-container">
         <div className="project-textbox">
             <div className="project-description">
-            {/* <img src="img/voicePlanet-1.png" alt="kpop" /> */}
-            <video muted autoPlay loop>
-                <source src="/img/voicePlanet.mp4" type="video/mp4" />
-            </video>
-            <br></br>
-            VOICE PLANET is a personalized voice recorder that links special moments to a specific place. 
-            With the VOICE PLANET, users can capture their special moments to a specific place and share them with their loved ones. 
-            They'll only be able to hear the voice message when they visit that exact same spot, creating a one-of-a-kind experience that's sure to leave a lasting impression.
-            <br></br>
-            <br></br>
-        </div>
+              <YouTube className="player"
+                  videoId="4kzjnaywr98"
+                  opts={{
+                    width: "100%",
+                    playerVars: {
+                      autoplay: 1,
+                      rel: 0,
+                      modestbranding: 1,
+                    },
+                  }}
+                  onEnd={(e)=>{e.target.stopVideo(0);}}      
+                />
+              <br></br>
+              VOICE PLANET is a personalized voice recorder that links special moments to a specific place. 
+              With the VOICE PLANET, users can capture their special moments to a specific place and share them with their loved ones. 
+              They'll only be able to hear the voice message when they visit that exact same spot, creating a one-of-a-kind experience that's sure to leave a lasting impression.
+              <br></br>
+              <br></br>
+            </div>
         </div>
       </div>
     </div>
