7ba3c01b346d44109436624770ed9e873117736c
janghanbee <jang010605@gmail.com>
2024-03-01 16:06:07 +0900

    added content of in:Planet

--- src/pages/project/inPlanet.js
+++ src/pages/project/inPlanet.js
@@ -7,6 +7,39 @@ const InPlanet = () => {
       <div className="project-container">
         <div className="project-textbox">
             <div className="project-description">
+            in:Plnaet ensures that investors are not trapped in their own view and encourages them to check information searched by other investors to eliminate confirmation bias effectively.
+            Furthermore, we offer information that was grounds for decision-making rather than simply others’ opinions or decisions to avoid community bias.
+            <br></br>
+            <br></br>
+            <img src="img/inPlanet-1.jpg" alt="inPlanet" />
+            <br></br>
+            <img src="img/inPlanet-2.jpg" alt="inPlanet" />
+            <br></br>
+            <img src="img/inPlanet-3.jpg" alt="inPlanet" />
+            <br></br>
+            <img src="img/inPlanet-4.jpg" alt="inPlanet" />
+            <br></br>
+            <img src="img/inPlanet-5.jpg" alt="inPlanet" />
+            <br></br>
+            <img src="img/inPlanet-6.jpg" alt="inPlanet" />
+            <br></br>
+            <img src="img/inPlanet-7.jpg" alt="inPlanet" />
+            <br></br>
+            <img src="img/inPlanet-8.jpg" alt="inPlanet" />
+            <br></br>
+            <img src="img/inPlanet-9.jpg" alt="inPlanet" />
+            <br></br>
+            <img src="img/inPlanet-10.jpg" alt="inPlanet" />
+            <br></br>
+            <img src="img/inPlanet-11.jpg" alt="inPlanet" />
+            <br></br>
+            <img src="img/inPlanet-12.jpg" alt="inPlanet" />
+            <br></br>
+            <img src="img/inPlanet-13.jpg" alt="inPlanet" />
+            <br></br>
+            <img src="img/inPlanet-14.jpg" alt="inPlanet" />
+            <br></br>
+            <img src="img/inPlanet-15.jpg" alt="inPlanet" />
             </div>
         </div>
       </div>
